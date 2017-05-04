b6999960e784f107328ddc68acc3e73e6ba98d46
“Akash <akashantony23@gmail.com>
2017-05-03 20:40:03 -0500

    made a file

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,35 @@
+# Method to create a list 
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps: 
+  # [fill in any steps here]
+  #take string.
+  #split it up into different words.
+#print output which will be in an array format.
+ 
+  # set default quantity
+  # print the list to the console [can you use one of your other methods here?]
+# output: [what data type goes here, array or hash?]
+
+# Method to add an item to a list
+# input: list, item name, and optional quantity
+# steps: make array into hash
+#add the item and quantity if any to the hash.
+# output: the output would be a hash with all details.
+
+# Method to remove an item from the list
+# input: the item to be removed,list
+# steps:identify item from within the hash and remove 
+# output:list without item.
+
+
+
+
+# Method to update the quantity of an item
+# input:list, item name, and quantity
+# steps:identify the item, the qty and update the value of qty from hash.
+# output:updated hash
+
+# Method to print a list and make it look pretty
+# input:
+# steps:
+# output:
